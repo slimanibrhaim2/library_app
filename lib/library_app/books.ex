7b470a99ac99d6ae3586144d7cea8d980f3e3703
sliman.ibrahim <sliman.ibrahim@hiast.edu.sy>
2025-11-20 12:28:23 +0000

    Add Search and Query Functions

--- lib/library_app/books.ex
+++ lib/library_app/books.ex
@@ -45,6 +45,43 @@ defmodule LibraryApp.Books do
     Book.changeset(book, attrs)
   end
 
+  # get available books
+  def list_available_books do
+    Book
+    |> where([b], b.available == true)
+    |> Repo.all()
+  end
 
+  # get by auther
+  def get_books_by_author(author) do
+    Book
+    |>where([b], ilike(b.author, ^"%#{author}%"))
+    |> Repo.all()
+  end
+
+  # general search for book
+  def search_books(search_term) do
+    search_pattern = "%#{search_term}%"
+    Book
+    |> where([b], ilike(b.title, ^search_pattern) or ilike(b.author, ^search_pattern))
+    |> Repo.all()
+  end
+
+  # get books by year
+  def get_books_by_year(year) do
+    Book
+    |> where([b], b.year == ^year)
+    |> Repo.all()
+  end
+
+  # get status about books how many available and how mane books exists
+  def get_books_status do
+    total_books = Repo.aggregate(Book, :count, :id)
+    available_book =
+      Book
+      |> where([b], b.available == true)
+      |> Repo.aggregate(:count, :id)
+    %{total: total_books, available: available_book}
+  end
 
 end
